e5b88af0602e76c789cf921558bf5190b004f00a
Ivana Pavic <ivana.pavic@dae.mn>
2021-10-26 11:48:43 +0100

    Upggrading for 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 locals {
-  b64_output_policies = "${split(",", lookup(data.external.policy_condenser.result, "b64_policies"))}"
+  b64_output_policies = split(",", data.external.policy_condenser.result["b64_policies"])
 }
 
 data "null_data_source" "policies" {
-  count = "${lookup(data.external.policy_condenser.result, "policy_count")}"
+  count = data.external.policy_condenser.result["policy_count"]
 
   inputs = {
-    policies = "${base64decode(element(local.b64_output_policies, count.index))}"
+    policies = base64decode(element(local.b64_output_policies, count.index))
   }
 }
 
 output "policies" {
   description = "A list of 1 or more IAM Policy documents in JSON format. These should be used with the aws_iam_policy resource to create the policies for the user, group or role."
-  value       = "${data.null_data_source.policies.*.outputs}"
+  value       = data.null_data_source.policies.*.outputs
 }
 
 output "policy_count" {
   description = "The number of policies being returned. This is to help avoid the dreaded Terraform 'value cannot be computed'."
-  value       = "${lookup(data.external.policy_condenser.result, "policy_count")}"
+  value       = data.external.policy_condenser.result["policy_count"]
 }
